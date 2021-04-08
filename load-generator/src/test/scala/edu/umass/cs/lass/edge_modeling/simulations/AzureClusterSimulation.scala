71a84d7653274d58e7b48e6b71b4c4e5ad36fc58
Bin Wang <wkk@users.noreply.github.com>
2021-04-07 23:10:42 -0400

    Fix bug

--- load-generator/src/test/scala/edu/umass/cs/lass/edge_modeling/simulations/AzureClusterSimulation.scala
+++ load-generator/src/test/scala/edu/umass/cs/lass/edge_modeling/simulations/AzureClusterSimulation.scala
@@ -1,13 +1,34 @@
 package edu.umass.cs.lass.edge_modeling.simulations
 
+import scala.concurrent.duration.DurationInt
 import io.gatling.core.Predef._
+import edu.umass.cs.lass.edge_modeling.scenarios.KerasRestApiScenario
+
+object AzureClusterSimulation {
+  val clusterExec = KerasRestApiScenario.kerasRestApiScenario
+    .inject(
+      rampUsers(2429) during(1 minute),
+      rampUsers(2473) during(1 minute),
+      rampUsers(2227) during(1 minute),
+      rampUsers(2515) during(1 minute),
+      rampUsers(2651) during(1 minute),
+      rampUsers(2602) during(1 minute),
+      rampUsers(2657) during(1 minute),
+      rampUsers(2599) during(1 minute),
+      rampUsers(2239) during(1 minute),
+      rampUsers(2307) during(1 minute),
+      rampUsers(2275) during(1 minute),
+      rampUsers(2446) during(1 minute),
+      rampUsers(2527) during(1 minute),
+      rampUsers(2456) during(1 minute),
+      rampUsers(2361) during(1 minute),
+      rampUsers(2353) during(1 minute),
+      rampUsers(2243) during(1 minute),
+      rampUsers(2483) during(1 minute),
+      rampUsers(2661) during(1 minute),
+      rampUsers(2442) during(1 minute))
+}
 
 class AzureClusterSimulation extends Simulation {
-  setUp(
-    AzureWorker1Simulation.worker1Exec,
-    AzureWorker2Simulation.worker2Exec,
-    AzureWorker3Simulation.worker3Exec,
-    AzureWorker4Simulation.worker4Exec,
-    AzureWorker5Simulation.worker5Exec
-  )
+  setUp(AzureClusterSimulation.clusterExec)
 }
